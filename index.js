c26c31a0cfc0ebf0cfd6891b5a495d2ba440b30f
gflo1997 <gonzalo.fj@hotmail.com>
2022-11-27 22:29:51 -0500

    Commit

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function contact(event) {
       .catch(() => {
         loading.classList.remove("modal__overlay--visible");
         alert(
-          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+          "The email service is temporarily unavailable. Please contact me directly on gonzalo.fj@hotmail.com"
         );
       });
   }
